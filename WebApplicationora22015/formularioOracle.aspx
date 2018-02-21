1e08cf27afe0063c6e44fc6e5c94c630805aef77
wagner cadena <wcadena@outlook.com>
2018-02-21 10:52:44 -0500

    primer commit

---
+++ WebApplicationora22015/formularioOracle.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="formularioOracle.aspx.cs" Inherits="WebApplicationora22015.formularioOracle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <h1>Mi primer Formulario</h1>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="EMPLOYEE_ID_lb" runat="server" Text="EMPLOYEE_ID"></asp:Label>
+        <asp:TextBox ID="EMPLOYEE_ID" runat="server"></asp:TextBox><br />
+        <asp:Label ID="FIRST_NAME_lb" runat="server" Text="FIRST_NAME"></asp:Label>
+        <asp:TextBox ID="FIRST_NAME" runat="server"></asp:TextBox><br />
+        <asp:Label ID="LAST_NAME_lb" runat="server" Text="LAST_NAME"></asp:Label>
+        <asp:TextBox ID="LAST_NAME" runat="server"></asp:TextBox><br />
+        <asp:Label ID="EMAIL_lb" runat="server" Text="EMAIL"></asp:Label>
+        <asp:TextBox ID="EMAIL" runat="server"></asp:TextBox><br />
+        <asp:Label ID="PHONE_NUMBER_lb" runat="server" Text="PHONE_NUMBER"></asp:Label>
+        <asp:TextBox ID="PHONE_NUMBER" runat="server"></asp:TextBox><br />
+        <asp:Label ID="HIRE_DATE_lb" runat="server" Text="HIRE_DATE"></asp:Label>
+        <asp:TextBox ID="HIRE_DATE" runat="server"></asp:TextBox><br />
+        <asp:Label ID="JOB_ID_lb" runat="server" Text="JOB_ID"></asp:Label>
+        <asp:TextBox ID="JOB_ID" runat="server"></asp:TextBox><br />
+        <asp:Label ID="SALARY_lb" runat="server" Text="SALARY"></asp:Label>
+        <asp:TextBox ID="SALARY" runat="server"></asp:TextBox><br />
+        <asp:Label ID="COMMISSION_PCT_lb" runat="server" Text="COMMISSION_PCT"></asp:Label>
+        <asp:TextBox ID="COMMISSION_PCT" runat="server"></asp:TextBox><br />
+        <asp:Label ID="MANAGER_ID_lb" runat="server" Text="MANAGER_ID"></asp:Label>
+        <asp:TextBox ID="MANAGER_ID" runat="server"></asp:TextBox><br />
+        <asp:Label ID="DEPARTMENT_ID_lb" runat="server" Text="DEPARTMENT_ID"></asp:Label>
+        <asp:TextBox ID="DEPARTMENT_ID" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="create" runat="server" Text="Nuevo" />  
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+        <asp:Button ID="read" runat="server" Text="Mostrar" />   
+        &nbsp;&nbsp;&nbsp;&nbsp;   
+        <asp:Button ID="update" runat="server" Text="Actualizar" />
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="delete" runat="server" Text="Borrar" />
+        <br />
+    </div>
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplicationora22015/formularioOracle.aspx.designer.cs
@@ -0,0 +1,267 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplicationora22015 {
+    
+    
+    public partial class formularioOracle {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control EMPLOYEE_ID_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EMPLOYEE_ID_lb;
+        
+        /// <summary>
+        /// Control EMPLOYEE_ID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EMPLOYEE_ID;
+        
+        /// <summary>
+        /// Control FIRST_NAME_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label FIRST_NAME_lb;
+        
+        /// <summary>
+        /// Control FIRST_NAME.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox FIRST_NAME;
+        
+        /// <summary>
+        /// Control LAST_NAME_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LAST_NAME_lb;
+        
+        /// <summary>
+        /// Control LAST_NAME.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox LAST_NAME;
+        
+        /// <summary>
+        /// Control EMAIL_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EMAIL_lb;
+        
+        /// <summary>
+        /// Control EMAIL.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EMAIL;
+        
+        /// <summary>
+        /// Control PHONE_NUMBER_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PHONE_NUMBER_lb;
+        
+        /// <summary>
+        /// Control PHONE_NUMBER.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PHONE_NUMBER;
+        
+        /// <summary>
+        /// Control HIRE_DATE_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HIRE_DATE_lb;
+        
+        /// <summary>
+        /// Control HIRE_DATE.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox HIRE_DATE;
+        
+        /// <summary>
+        /// Control JOB_ID_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label JOB_ID_lb;
+        
+        /// <summary>
+        /// Control JOB_ID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox JOB_ID;
+        
+        /// <summary>
+        /// Control SALARY_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label SALARY_lb;
+        
+        /// <summary>
+        /// Control SALARY.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox SALARY;
+        
+        /// <summary>
+        /// Control COMMISSION_PCT_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label COMMISSION_PCT_lb;
+        
+        /// <summary>
+        /// Control COMMISSION_PCT.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox COMMISSION_PCT;
+        
+        /// <summary>
+        /// Control MANAGER_ID_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label MANAGER_ID_lb;
+        
+        /// <summary>
+        /// Control MANAGER_ID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MANAGER_ID;
+        
+        /// <summary>
+        /// Control DEPARTMENT_ID_lb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DEPARTMENT_ID_lb;
+        
+        /// <summary>
+        /// Control DEPARTMENT_ID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox DEPARTMENT_ID;
+        
+        /// <summary>
+        /// Control create.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button create;
+        
+        /// <summary>
+        /// Control read.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button read;
+        
+        /// <summary>
+        /// Control update.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button update;
+        
+        /// <summary>
+        /// Control delete.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button delete;
+        
+        /// <summary>
+        /// Control GridView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+    }
+}
